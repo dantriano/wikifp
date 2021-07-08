f1c118355e7000e9d0245d27de8567147ec2e6b5
Dan <dan.triano@gmail.com>
2021-07-08 04:58:58 +0200

    DB server and data folder

--- docker/mysql/init.sql
+++
@@ -1,5 +0,0 @@
-show databases;
-CREATE DATABASE IF NOT EXISTS laravel;
-CREATE USER  IF NOT EXISTS 'laravel'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON laravel.* TO 'laravel'@'%';
-FLUSH PRIVILEGES;
